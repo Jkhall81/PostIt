7755ada6789a4c4c41b7d8ab276620f35f14479e
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-11 09:57:26 -0700

    new users automatically get a profile, and automatically will follow their own profile.

--- social/postit/models.py
+++ social/postit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create user profile model
@@ -14,3 +15,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# Create profile when new user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # have user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
