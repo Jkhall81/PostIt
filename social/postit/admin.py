2199228bf7789f94940098d0030e040b7d68828c
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-11 07:28:35 -0700

    added backend profile feature for users, and ability to follow other users.  onetoone relationship between a user and their profile, and a manytomany nonsymmetrical relationship for following.

--- social/postit/admin.py
+++ social/postit/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+
+# mix profile info into user section
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # just display username fields on admin page
+    fields = ['username']
+    inlines = [ProfileInline]
+
+
+# unregister initial user
+admin.site.unregister(User)
+
+# register and profile
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
--- social/postit/models.py
+++ social/postit/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create user profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
