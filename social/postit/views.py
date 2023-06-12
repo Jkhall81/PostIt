10012636eca4a1cb5fc6762584b1e2b33b34477f
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-12 06:10:03 -0700

    added buttons to follow and unfollow other users, added code to make them work.

--- social/postit/views.py
+++ social/postit/views.py
@@ -19,6 +19,21 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+        # post form logic
+        if request.method == 'POST':
+            # get current user
+            current_user_profile = request.user.profile
+            # get form data
+            action = request.POST['follow']
+            # decide to follow or unfollow
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            # save the profile
+            current_user_profile.save()
+
         return render(request, 'profile.html', {'profile': profile})
     else:
         messages.success(request, 'You must be logged in to view this page!')
