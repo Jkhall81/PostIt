2f67ed9230597db3a6ec9409d4f45e658d5d687d
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-12 05:48:11 -0700

    added links to the profile list page, and tweaked the layout on the profile pages a little bit.  Put the followed and followed by info in seperate cards.

--- social/postit/views.py
+++ social/postit/views.py
@@ -15,3 +15,11 @@ def profile_list(request):
         messages.success(request, 'You must be logged in to view this page!')
         return redirect('home')
 
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'profile.html', {'profile': profile})
+    else:
+        messages.success(request, 'You must be logged in to view this page!')
+        return redirect('home')
