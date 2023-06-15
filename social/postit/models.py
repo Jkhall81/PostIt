f5f48c60de95eb051a4bb29c07fc7ce1d0b5898b
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-14 20:32:20 -0700

    added posts to the admin, posts display now on the home page, and each users profile page displays their posts.

--- social/postit/models.py
+++ social/postit/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
+# create post model
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, related_name='posts',
+        on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(
+            f'{self.user} '
+            f'({self.created_at:%Y-%m-%d %H:%M}): '
+            f'{self.body}...'
+        )
+
+
 # Create user profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- social/postit/views.py
+++ social/postit/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import Profile
+from .models import Profile, Post
 from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
 
 
 def profile_list(request):
@@ -19,6 +21,7 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        posts = Post.objects.filter(user_id=pk).order_by('-created_at')
 
         # post form logic
         if request.method == 'POST':
@@ -34,7 +37,7 @@ def profile(request, pk):
             # save the profile
             current_user_profile.save()
 
-        return render(request, 'profile.html', {'profile': profile})
+        return render(request, 'profile.html', {'profile': profile, 'posts': posts})
     else:
         messages.success(request, 'You must be logged in to view this page!')
         return redirect('home')
