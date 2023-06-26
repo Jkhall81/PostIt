7f60b2a215191ade2d381f8642e715c212b1434f
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-25 17:05:39 -0700

    deploy test v3.1.

--- social/social/settings.py
+++ social/social/settings.py
@@ -116,7 +116,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
